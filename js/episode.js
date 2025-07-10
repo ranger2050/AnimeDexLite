ba4e72b17f92b9a3717f5c0a6e2a8dc793a5878b
ranger2050 <dran3000@proton.me>
2025-07-10 01:21:26 -0400

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -1,5 +1,5 @@
 // Api urls
-const ProxyApi = "https://https://cors-proxy.dran3000.workers.dev/?u=";
+const ProxyApi = "https://cors-proxy.dran3000.workers.dev/?u=";
 const animeapi = "/anime/";
 const episodeapi = "/episode/";
 const dlapi = "/download/";
