72b485bf3a4445997c9438e8079f644b31f7d187
ranger2050 <dran3000@proton.me>
2025-07-10 01:07:27 -0400

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=";
+const ProxyApi = "https://https://cors-proxy.dran3000.workers.dev/?u=";
 const animeapi = "/anime/";
 const recommendationsapi = "/recommendations/";
 
