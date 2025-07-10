2ea7160f0e23ccd66cd1f870d36380a04860b0b0
ranger2050 <dran3000@proton.me>
2025-07-10 01:20:59 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://https://cors-proxy.dran3000.workers.dev/?u=";
+const ProxyApi = "https://cors-proxy.dran3000.workers.dev/?u=";
 const searchapi = "/search/";
 
 // Api Server Manager
